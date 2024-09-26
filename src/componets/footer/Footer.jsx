738a62d5c70a4fb4cc932afe4948ec4b2cdab7c4
rajashree104 <rajashreesahoo944@gmail.com>
2024-09-26 12:38:52 +0530

    footer

--- src/componets/footer/Footer.jsx
+++ src/componets/footer/Footer.jsx
@@ -1,18 +1,20 @@
 import { FaFacebook } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa6";
-import { FaLocationDot } from "react-icons/fa6";
-import { MdEmail } from "react-icons/md";
-import { FaPhone } from "react-icons/fa6";
+// import { FaLocationDot } from "react-icons/fa6";
+// import { MdEmail } from "react-icons/md";
+// import { FaPhone } from "react-icons/fa6";
 
 import './Footer.css'
 const Footer = () => {
   return (
     <>
       <footer id="footer">
-        
-
-        
+       <div className="footer_social_container">
+         <FaFacebook className="footer_icon"/>
+         <FaInstagram   className="footer_icon"/>
+         <FaLinkedin   className="footer_icon"/>
+            </div>
         <div className="footer_buttom">
           © 2000-2024, NUEZ Technologies Pvt Ltd. All right reserved.
         </div>
