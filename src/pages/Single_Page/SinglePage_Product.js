e485f3ea3f587a7ea1b3d7fe62df1a462d66618a
Jagruti Priyadarshini <jagrutipriyadarshini246@gmail.com>
2024-09-27 12:23:29 +0530

    single_page

---
+++ src/pages/Single_Page/SinglePage_Product.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// Indivisual single product section
+const ProductSection = ({ heading, image, contents }) => (
+  <div className=" single_product_section ">
+ <div className="single_product_heading"><h2>{heading}</h2></div>
+<div className="single_product_container">
+    <div className="single_product_section_image">
+      <img src={image} alt={heading} className="image" />
+    </div>
+    <div className="single_product_section_text">
+      <ul>
+        {contents.map((content, index) => (
+          <li key={index} dangerouslySetInnerHTML={{ __html: content }} />
+        ))}
+      </ul>
+    </div>
+    
+    </div>
+  </div>
+);
+
+export default ProductSection;
+
+
